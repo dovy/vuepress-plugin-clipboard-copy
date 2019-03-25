b37d78711587eaf1c17dd64f019a134cde1eceaa
Dovy Paukstys <info@simplerain.com>
2019-03-25 15:50:20 -0400

    Conflict

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options, context) => ({
   define: {
-    SELECTOR: options.selector || 'div[class*="language-"] pre'
+    COPY_SELECTOR: options.copy_selector || 'div[class*="language-"] pre'
   },
   clientRootMixin: path.resolve(__dirname, 'clientRootMixin.js')
 })
